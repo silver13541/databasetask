823b5e4879fd4ba0f79294b429fcab822f589698
silver13541 <elvar.halilov@mail.ru>
2021-08-02 22:27:35 +0400

    fix index.js

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ const connection = mysql.createConnection({
   password: "RcCrZkDPTA",
 });
 
-app.get("/", function (request, responce) {
+app.post("/", function (request, responce) {
   connection.query("SELECT * FROM Countries", (err, result, fields) => {
     if (err) console.log(err);
     responce.send(result) 
   });
 });
 
-app.get("/cities", function (request, responce) {
+app.post("/cities", function (request, responce) {
   connection.query("SELECT C.*,CO.Name,CO.Coordinates FROM Cities as C,Countries as CO WHERE C.country_id=CO.id", (err, result, fields) => {
     console.log(result);
     if (err) console.log(err);
